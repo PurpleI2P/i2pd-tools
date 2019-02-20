0dabbf67989cac5cf77daef8ba773c795e3b1a94
orignal <i2porignal@yandex.ru>
2019-02-20 12:59:27 -0500

    show offline signature expiration time and trasient signature type

--- keyinfo.cpp
+++ keyinfo.cpp
@@ -1,11 +1,22 @@
 #include "Identity.h"
+#include "I2PEndian.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <unistd.h>
+#include <stdio.h>
+#include <time.h>
 #include "common/key.hpp"
 
+
+std::string ConvertTime (time_t t)
+{
+	struct tm *tm = localtime(&t);
+	char date[128];
+	snprintf(date, sizeof(date), "%02d/%02d/%d %02d:%02d:%02d", tm->tm_mday, tm->tm_mon + 1, tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);
+	return date;
+}
 
 int main(int argc, char * argv[])
 {
@@ -62,7 +73,13 @@ int main(int argc, char * argv[])
     std::cout << "B32 Address: " << ident.ToBase32() << ".b32.i2p" << std::endl;
     std::cout << "Signature Type: " << SigTypeToName(dest->GetSigningKeyType()) << std::endl;
     std::cout << "Encryption Type: " << (int) dest->GetCryptoKeyType() << std::endl;
-	if (keys.IsOfflineSignature ()) std::cout << "Offline signature" << std::endl;
+	if (keys.IsOfflineSignature ())
+	{	 
+		std::cout << "Offline signature" << std::endl;
+		const auto& offlineSignature = keys.GetOfflineSignature ();
+		std::cout << "Expires: " << ConvertTime (bufbe32toh(offlineSignature.data ())) << std::endl;
+		std::cout << "Transient Signature Type: " << SigTypeToName(bufbe16toh(offlineSignature.data () + 4)) << std::endl;	
+	}
   } else {
     if(print_full) {
       std::cout << dest->ToBase64() << std::endl;
