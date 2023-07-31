114fd1cfe701425c8b5f363e06a790981901eb40
wipedlifepotato <startyourday60@gmail.com>
2023-07-31 22:18:34 +0700

    LSB Release instead uname if we can

--- dependencies.sh
+++ dependencies.sh
@@ -14,14 +14,55 @@ dependWin="mingw-w64-x86_64-boost \
     mingw-w64-x86_64-zlib"
 
 kernel=`uname -a`
+function isLsbReleaseExists() {
+  if which lsb_release > /dev/null; # do whereis can be too?
+  then
+	  return 0
+  else
+	  return 1
+  fi
+}
 
-case "$kernel" in
-*Ubuntu*|*Debian*)
-	sudo apt install $dependNix;;
-*gentoo*)
-	sudo emerge --deep --newuse dev-libs/boost dev-libs/openssl;;
-*MINGW64*)
-	pacman -S $dependWin;;
-*)
-	echo "Just install libboost and libopenssl dev packages on your pc";;
+function anotherDistr(){
+ 	echo "Just install libboost and libopenssl dev packages on your pc";
+	return 0
+}
+
+function installDeb(){
+	 sudo apt-get install $dependNix;
+	 return 0
+}
+function installOnGentoo(){
+		sudo emerge --deep --newuse dev-libs/boost dev-libs/openssl;
+		return 0
+}
+function installOnWin() {
+		pacman -S $depenWin;
+		return 0
+}
+
+function doInstallDepencies() {
+case "$1" in
+	*Ubuntu*|*Debian*)
+		installDeb
+		;;
+        *gentoo*)
+		installOnGentoo
+		;;
+	*MINGW64*)
+		installOnWin
+		;;
+	*)
+		anotherDistr
+		;;
 esac
+}
+
+if isLsbReleaseExists ;
+then
+ distr=`lsb_release -i`
+ doInstallDepencies "$distr"
+else #
+ doInstallDepencies "$kernel"
+fi
+
